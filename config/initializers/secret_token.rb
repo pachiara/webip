1f2668d992bd4f6fd17a6277e4a903436b2205c4
pachiara <chiara.palmieri@gmail.com>
2015-10-19 12:20:16 +0200

    gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webip::Application.config.secret_token = '195af7db0afe1bb55be5ad53ce28e16fa06cfd41f453316fa423a5e093e24044336bb43bbbf89f701e5807b5bb5364c4fe5d8cc38de1719eb807c2bf74279aa4'
