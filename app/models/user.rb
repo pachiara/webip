5b3ca19f3c2258da08032f357072f65710e8328d
pachiara <chiara.palmieri@gmail.com>
2015-10-29 11:22:47 +0100

    update devise 3.5.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  #  devise :database_authenticatable, :registerable,
-  #         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable
 
   devise :database_authenticatable, :registerable, :recoverable
 
-  
-#  devise :database_authenticatable, :trackable, :timeoutable,
-#         :timeout_in => 20.minutes
-
-  # Setup accessible (or protected) attributes for your model
- # attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
-  
- # validate :email, :uniqueness => true
-  
 end
