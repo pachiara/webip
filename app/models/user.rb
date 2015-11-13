9b9d6da1ff590e5d3c9a5699c7e49db7b59a96b0
pachiara <chiara.palmieri@gmail.com>
2015-11-13 15:50:02 +0100

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   #devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :trackable, :validatable
 
-  devise :database_authenticatable, :registerable, :recoverable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
 end
